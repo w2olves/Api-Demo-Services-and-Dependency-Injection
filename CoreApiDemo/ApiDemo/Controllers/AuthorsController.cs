2c6235d07e152962da153a59cefdea60a8f041ba
w2olves <w2olves@hotmail.com>
2017-04-20 11:07:04 -0500

    Add files via upload

---
+++ CoreApiDemo/ApiDemo/Controllers/AuthorsController.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiDemo.Controllers
+{
+    [Route("api/Authors")]
+    [EnableCors("CorsPolicy")]
+    public class AuthorsController : Controller
+    {
+        [HttpGet()]
+        public IActionResult GetAuthors()
+        {
+
+            return Ok(AuthorsDatastore.Current.Authors);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetAuthor(int id)
+        {
+            var authorToReturn = AuthorsDatastore.Current.Authors.FirstOrDefault(c => c.Id == id);
+            if (authorToReturn == null)
+            {
+                return NotFound("This author ID is invalid");
+            }
+            return Ok(authorToReturn);
+        }
+
+        [HttpGet("GetAuthorByName/{name}", Name = "GetAuthorByName")]
+        public IActionResult GetAuthorByName(string name)
+        {
+            var authorToReturn = AuthorsDatastore.Current.Authors.FirstOrDefault(c => c.Name.Contains(name));
+            if (authorToReturn == null)
+            {
+                return NotFound("There is no Author by that name");
+                }
+            return Ok(authorToReturn);
+        }
+    }
+}
---
+++ CoreApiDemo/ApiDemo/Startup.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Cors.Internal;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace ApiDemo
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add service and create Policy with options
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials());
+            });
+
+            // Add framework services.
+            services.AddMvc()
+            //.AddMvcOptions(o => o.OutputFormatters.Add(
+            //new XmlDataContractSerializerOutputFormatter()))
+            .AddJsonOptions(o =>
+            {
+                if (o.SerializerSettings.ContractResolver != null)
+                {
+                    var castedResolver = o.SerializerSettings.ContractResolver as DefaultContractResolver;
+                    castedResolver.NamingStrategy = null;
+                }
+            });
+
+            //Configure MVC
+            services.Configure<MvcOptions>(options =>
+            {
+                options.Filters.Add(new CorsAuthorizationFilterFactory("AllowAllOrigins"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.UseMvc();
+        }
+    }
+}
