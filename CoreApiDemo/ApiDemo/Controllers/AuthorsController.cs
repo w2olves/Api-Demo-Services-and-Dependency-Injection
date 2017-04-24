12b2a1bde04fe359d12cc6058512463498fe5c7c
Abhijit Chatterjee <AC17435@homedefenders.com>
2017-04-24 12:22:13 -0400

    Added Author Creation Method

--- CoreApiDemo/ApiDemo/Controllers/AuthorsController.cs
+++ CoreApiDemo/ApiDemo/Controllers/AuthorsController.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ApiDemo.Models;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -19,7 +20,7 @@ namespace ApiDemo.Controllers
             return Ok(AuthorsDatastore.Current.Authors);
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id}", Name = "GetAuthor")]
         public IActionResult GetAuthor(int id)
         {
             var authorToReturn = AuthorsDatastore.Current.Authors.FirstOrDefault(c => c.Id == id);
@@ -39,6 +40,53 @@ namespace ApiDemo.Controllers
                 return NotFound("There is no Author by that name");
                 }
             return Ok(authorToReturn);
+        }
+
+        [HttpPost("CreateAuthor/{name}", Name = "CreateAuthor")]
+        public IActionResult CreateAuthor([FromBody] AuthorsForCreationDto author)
+        {
+            //Validations
+            if (author == null)
+            {
+                return BadRequest("No Author Info available");
+            }
+
+            if (author.Name == author.Description)
+            {
+                ModelState.AddModelError("Description", "The author name and the description cannot be the same");
+            }
+
+            //Is the model in a valid state
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+           
+            //Demo Purpose will be improved
+            //Get Max Book Id
+            var maxAuthorId = AuthorsDatastore.Current.Authors.Select(a => a.Id).Max();
+            //The above method is slow 
+            //The above method does not take into account scenarios where multiple users will try to get an ID simultaneously
+
+
+            var newAuthor = new AuthorDto()
+            {
+                Id = ++maxAuthorId,
+                Name = author.Name,
+                Description = author.Description,
+                Books = author.Books
+            };
+
+            AuthorsDatastore.Current.Authors.Add(newAuthor);
+
+            //return Ok(author.Books);
+
+            //For Post the advised response is 201 Created
+              return CreatedAtRoute("GetAuthor", new { id = newAuthor.Id }, newAuthor);
+
+          //  return Ok(newAuthor);
+
         }
     }
 }
---
+++ CoreApiDemo/ApiDemo/Models/AuthorsForCreationDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiDemo.Models
+{
+    public class AuthorsForCreationDto
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public int NumberOfBooks
+        {
+            get
+            {
+                return Books.Count;
+            }
+        }
+
+        public ICollection<BooksDto> Books { get; set; } = new List<BooksDto>();
+    }
+}
